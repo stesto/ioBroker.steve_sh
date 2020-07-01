743d3e4a7d19b4a34c66b3fc3d463078f427c615
stesto <s.stodder@hotmail.de>
2020-07-01 11:47:42 +0200

    Initial commit

---
+++ admin/words.js
@@ -0,0 +1,41 @@
+/*global systemDictionary:true */
+"use strict";
+
+systemDictionary = {
+    "steve_sh adapter settings": {
+        en: "Adapter settings for steve_sh",
+        de: "Adaptereinstellungen für steve_sh",
+        ru: "Настройки адаптера для steve_sh",
+        pt: "Configurações do adaptador para steve_sh",
+        nl: "Adapterinstellingen voor steve_sh",
+        fr: "Paramètres d'adaptateur pour steve_sh",
+        it: "Impostazioni dell'adattatore per steve_sh",
+        es: "Ajustes del adaptador para steve_sh",
+        pl: "Ustawienia adaptera dla steve_sh",
+        "zh-cn": "steve_sh的适配器设置",
+    },
+    option1: {
+        en: "option1",
+        de: "Option 1",
+        ru: "Опция 1",
+        pt: "Opção 1",
+        nl: "Optie 1",
+        fr: "Option 1",
+        it: "opzione 1",
+        es: "Opción 1",
+        pl: "opcja 1",
+        "zh-cn": "选项1",
+    },
+    option2: {
+        en: "option2",
+        de: "Option 2",
+        ru: "option2",
+        pt: "opção 2",
+        nl: "Optie 2",
+        fr: "Option 2",
+        it: "opzione 2",
+        es: "opcion 2",
+        pl: "Opcja 2",
+        "zh-cn": "选项2",
+    },
+};
